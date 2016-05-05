0249f6359e24f398dbbe775e8d5d87efa9e28a08
landama2 <landama2@users.noreply.github.com>
2016-05-05 18:48:47 +0200

    Pridani generickeho DAO

---
+++ src/main/java/DAO/StudentDAO.java
@@ -0,0 +1,11 @@
+package DAO;
+
+/**
+ * Created by marek on 4.5.16.
+ */
+public class StudentDAO extends GenericDAO {
+
+    public StudentDAO(Class entityClass) {
+        super(entityClass);
+    }
+}
