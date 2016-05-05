4c5e16a5ae73eab0d62c9b864182783547cd6a63
urbanm30 <urbanm30@fel.cvut.cz>
2016-05-05 22:57:13 +0200

    Added basics of the GUI form

--- src/main/java/form.java
+++ src/main/java/form.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+
 /**
  * Created by urban on 5/4/2016.
  */
 public class form {
+    private JTabbedPane TabPanel;
+    private JPanel Panel;
+    private JTextField textField1;
+    private JButton pridatStudentaButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JList list1;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JComboBox comboBox2;
+    private JButton zapsatPredmetButton;
+    private JTextField textField6;
+    private JComboBox comboBox3;
+    private JTree seznam_terminu;
+    private JTextArea coJaVimTextArea;
+    private JTextField podleKoduTextField;
+    private JTextField textField8;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
